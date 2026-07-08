ffc8ee9e0b380b0ebbe38d8db68e104ae0a715b0
Cesar Chirino <tito1718@aol.com>
2026-07-07 17:34:51 -0700

    Create database models

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const { PORT = 3001 } = process.env;
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
