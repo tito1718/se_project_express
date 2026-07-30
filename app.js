9e1cd6159d69e213933e23c9a1b5482eae906ca3
Cesar Chirino <tito1718@aol.com>
2026-07-29 20:09:29 -0700

    Add temporary user middleware

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+
+  next();
+});
+
 app.use("/", routes);
 
 app.listen(PORT, () => {
