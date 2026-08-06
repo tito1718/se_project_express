433f2177a0a0390cf0ad04fc670149c5d70b1955
Cesar Chirino <tito1718@aol.com>
2026-08-05 21:02:07 -0700

    Add request and error logging

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const { errors } = require("celebrate");
 
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+
 const routes = require("./routes");
 const errorHandler = require("./middlewares/error-handler");
 const NotFoundError = require("./errors/not-found-error");
@@ -15,6 +17,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use(cors());
 app.use(express.json());
+
+app.use(requestLogger);
 
 app.use((req, res, next) => {
   req.user = {
@@ -29,6 +33,8 @@ app.use("/", routes);
 app.use((_req, _res, next) => {
   next(new NotFoundError("Requested resource not found"));
 });
+
+app.use(errorLogger);
 
 app.use(errors());
 
